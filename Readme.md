4bdcc90f12bfafb00da96fa540cd857c14d16759
Choi <EGchoi.dev@gmail.com>
2022-03-29 17:26:09 +0900

    chapter01 - Firebase Message - Complete

--- Readme.md
+++ Readme.md
@@ -13,11 +13,14 @@ FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
     }
 }
 ```
-- [메시지 전송 테스트 페이지](https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send)에서 테스트하기
-![테스트 형식](./resources/try_send_message.png)
+
+- [메시지 전송 테스트 페이지](https://console.firebase.google.com/project/aop-part3-chapter01-25e62/notification/compose)에서 테스트하기
+![테스트 형식](./resources/try_notification.png)
 
 
 ## FirebaseMessagingService
 - `FirebaseMessagingService` 서비스 등록을 통해 메시지와 토큰을 처리한다.
 - `onNewToken` 오버라이드 함수를 통해 토큰이 변경 됐을 때 이벤트를 처리한다. ([토큰이 변경되는 경우](https://firebase.google.com/docs/cloud-messaging/android/first-message?hl=ko&authuser=0#access_the_registration_token))
 - `onMessageReceived` 오버라이드 함수를 통해 메시지를 받았을 때 이벤트를 처리한다.
+- `onMessageReceived`함수 내에 중단점을 걸고 [메시지 전송 테스트 페이지](https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send)에서 테스트 메시지 전송하기
+![테스트 형식](./resources/try_send_message.png)
--- aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
+++ aoppart3chapter01/app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.aop.part3.chapter01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "로그"
 
-    private val binding : ActivityMainBinding by lazy {
+    private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
@@ -20,18 +21,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initFirebase()
+        updateResult()
     }
 
-    private fun initFirebase(){
+    // Intent.FLAG_ACTIVITY_SINGLE_TOP 플래그로 액티비티가 호출됐을 때 실행되는 함수
+    // https://developer.android.com/guide/components/activities/tasks-and-back-stack?hl=ko#ManagingTasks
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun initFirebase() {
 
 //        val firebaseApp = FirebaseApp.initializeApp(this)
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if(task.isSuccessful){
+            if (task.isSuccessful) {
                 val token = task.result
                 binding.firebaseTokenTextView.text = token
                 Log.d(TAG, "MainActivity - initFirebase - token : $token")
             }
         }
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        binding.resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런쳐") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
     }
 }
