cfe513c3c68d69fba815d14076fcb91269dbba80
egchoi <egchoi.dev@gmail.com>
2022-04-05 21:03:14 +0900

    chapter05 - Tinder - Facebook Login

--- aoppart3chapter05/app/src/main/java/fastcampus/aop/part3/chapter05/LoginActivity.kt
+++ aoppart3chapter05/app/src/main/java/fastcampus/aop/part3/chapter05/LoginActivity.kt
@@ -1,9 +1,15 @@
 package fastcampus.aop.part3.chapter05
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -19,6 +25,11 @@ class LoginActivity : AppCompatActivity() {
         ActivityLoginBinding.inflate(layoutInflater)
     }
 
+    private val callbackManager: CallbackManager by lazy {
+        CallbackManager.Factory.create()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -26,8 +37,8 @@ class LoginActivity : AppCompatActivity() {
         initLoginButton()
         initSignUpButton()
         initEmailAndPasswordEditText()
+        initFacebookLoginButton()
     }
-
 
 
     private fun initLoginButton() {
@@ -40,7 +51,11 @@ class LoginActivity : AppCompatActivity() {
                     if (task.isSuccessful) {
                         finish()
                     } else {
-                        Toast.makeText(this, "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            this,
+                            "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
         }
@@ -53,10 +68,15 @@ class LoginActivity : AppCompatActivity() {
 
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
-                    if(task.isSuccessful){
-                        Toast.makeText(this, "회원강비에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원강비에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     } else {
-                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
         }
@@ -64,19 +84,54 @@ class LoginActivity : AppCompatActivity() {
 
     private fun initEmailAndPasswordEditText() {
         binding.emailEditText.addTextChangedListener {
-            val enable = binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()
+            val enable =
+                binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()
             binding.loginButton.isEnabled = enable
             binding.signUpButton.isEnabled = enable
         }
         binding.passwordEditText.addTextChangedListener {
-            val enable = binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()
+            val enable =
+                binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()
             binding.loginButton.isEnabled = enable
             binding.signUpButton.isEnabled = enable
         }
     }
 
+    private fun initFacebookLoginButton() {
+        binding.facebookLoginButton.setPermissions("email", "public_profile")
+        binding.facebookLoginButton.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onCancel() {}
+
+                override fun onError(error: FacebookException) {
+                    Toast.makeText(this@LoginActivity, "페이스북 로그인 실패", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener(this@LoginActivity) { task ->
+                            if(task.isSuccessful){
+                                finish()
+                            } else {
+                                Toast.makeText(this@LoginActivity, "페이스북 로그인 실패", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+
+            })
+    }
+
 
     private fun getInputEmail() = binding.emailEditText.text.toString()
     private fun getInputPassword() = binding.passwordEditText.text.toString()
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 
 }
