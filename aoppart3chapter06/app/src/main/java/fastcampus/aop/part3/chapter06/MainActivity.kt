d71e3fd042611368e4310565267b8b1062e8992c
egchoi <egchoi.dev@gmail.com>
2022-04-12 20:36:12 +0900

    chapter06 - Jonggo - chageFragment

--- aoppart3chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/MainActivity.kt
+++ aoppart3chapter06/app/src/main/java/fastcampus/aop/part3/chapter06/MainActivity.kt
@@ -2,7 +2,12 @@ package fastcampus.aop.part3.chapter06
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import fastcampus.aop.part3.chapter06.chatlist.ChatListFragment
 import fastcampus.aop.part3.chapter06.databinding.ActivityMainBinding
+import fastcampus.aop.part3.chapter06.home.HomeFragment
+import fastcampus.aop.part3.chapter06.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +15,37 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val homeFragment = HomeFragment()
+    private val charListFragment = ChatListFragment()
+    private val myPageFragment = MyPageFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        replaceFragment(homeFragment)
+        initBottomNavigationView()
+
     }
+
+    private fun initBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.home -> replaceFragment(homeFragment)
+                R.id.chatList-> replaceFragment(charListFragment)
+                R.id.myPage ->replaceFragment(myPageFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(binding.fragmentContainer.id, fragment)
+                commit()
+            }
+    }
+
 }
+
