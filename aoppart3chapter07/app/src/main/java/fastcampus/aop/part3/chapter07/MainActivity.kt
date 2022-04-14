2d03705d141a668d534a9523652f86870d4e4c0b
egchoi <egchoi.dev@gmail.com>
2022-04-14 21:44:04 +0900

    chapter07 - Airbnb - my location and marker

--- aoppart3chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
+++ aoppart3chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -3,10 +3,10 @@ package fastcampus.aop.part3.chapter07
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.CameraUpdate
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.UiSettings
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 import fastcampus.aop.part3.chapter07.databinding.ActivityMainBinding
 
 
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var locationSource: FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +24,50 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.mapView.onCreate(savedInstanceState)
 
         binding.mapView.getMapAsync(this)
+    }
+
+
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.4796889, 126.9136327))
+        naverMap.moveCamera(cameraUpdate)
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        val marker = Marker()
+        marker.position = LatLng(37.487563, 126.913346)
+        marker.map = naverMap
+//        marker.icon = MarkerIcons.BLUE // https://navermaps.github.io/android-map-sdk/guide-ko/5-2.html 마커 속성 가이드
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+
     }
 
     override fun onStart() {
@@ -60,19 +105,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding.mapView.onLowMemory()
     }
 
-    override fun onMapReady(map: NaverMap) {
-        naverMap = map
 
-        naverMap.maxZoom = 18.0
-        naverMap.minZoom = 10.0
-
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.4796889,126.9136327))
-        naverMap.moveCamera(cameraUpdate)
-
-        val uiSettings = naverMap.uiSettings
-        uiSettings.isLocationButtonEnabled = true
-
-
-        requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
+
 }
