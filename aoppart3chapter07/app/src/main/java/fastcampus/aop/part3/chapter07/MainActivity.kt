1e9be1cf156fd11331fd953dabf78bfa01af5fe9
Choi <EGchoi.dev@gmail.com>
2022-04-14 17:19:28 +0900

    chapter06 - Airbnb - init naver map

---
+++ aoppart3chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -0,0 +1,54 @@
+package fastcampus.aop.part3.chapter07
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fastcampus.aop.part3.chapter07.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+}
