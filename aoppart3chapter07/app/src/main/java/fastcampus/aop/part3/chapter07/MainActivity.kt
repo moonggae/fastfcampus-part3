99d683ab827f5965aed9c5abad0919346cd0b46b
Choi <EGchoi.dev@gmail.com>
2022-04-14 17:31:46 +0900

    chapter06 - Airbnb - setting naver map

--- aoppart3chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
+++ aoppart3chapter07/app/src/main/java/fastcampus/aop/part3/chapter07/MainActivity.kt
@@ -2,11 +2,17 @@ package fastcampus.aop.part3.chapter07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.UiSettings
 import fastcampus.aop.part3.chapter07.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private lateinit var naverMap: NaverMap
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -15,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync(this)
     }
 
     override fun onStart() {
@@ -50,5 +58,21 @@ class MainActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         binding.mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.4796889,126.9136327))
+        naverMap.moveCamera(cameraUpdate)
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+
+
+        requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1000)
     }
 }
